5fc0aed2732dca9810d77d00b77c2e57516745ab
s-kosugi <program.sk5@outlook.jp>
2020-11-16 19:53:45 +0900

    ItemManagerとEnemyManagerでの出現制御時にゲームプレイ状態を参照するようにした。

--- Project/test2D/Assets/Enemy/EnemyManager.cs
+++ Project/test2D/Assets/Enemy/EnemyManager.cs
@@ -3,6 +3,7 @@
 public class EnemyManager : MonoBehaviour
 {
     private CameraController m_Camera = null;
+    private GameMainManager m_GameMain = null;
     static float SPRITE_SIZE = 32.0f;
 
     private EnemyPopTable m_Table = null;
@@ -15,6 +16,7 @@ public class EnemyManager : MonoBehaviour
     void Start()
     {
         m_Camera = GameObject.Find("Main Camera").GetComponent<Camera>().GetComponent<CameraController>();
+        m_GameMain = GameObject.Find("GameManager").GetComponent<GameMainManager>();
 
         // 敵出現テーブルの0を読み込む
         LoadTable(0);
@@ -23,28 +25,32 @@ public class EnemyManager : MonoBehaviour
 
     void Update()
     {
-        // 出現テーブルから敵を出現させる
-        foreach( EnemyTableItem item in m_Table.EnemyTableItemList)
+        // ゲームプレイ中のみ敵を出現させる
+        if (m_GameMain.state == GameMainManager.STATE.MAIN)
         {
-            item.Elapsed += Time.deltaTime;
-            if( item.INTERVAL <= item.Elapsed)
+            // 出現テーブルから敵を出現させる
+            foreach (EnemyTableItem item in m_Table.EnemyTableItemList)
             {
-                item.Elapsed = 0;
-                // 座標をランダムに決める
-                Vector3 pos;
-                pos.x = m_Camera.GetScreenRight() + SPRITE_SIZE / 2.0f;
-                pos.y = Random.Range(m_Camera.GetScreenTop(), m_Camera.GetScreenBottom());
-                pos.z = 0.0f;
-                GameObject popEnemy = null;
-
-                switch (item.ID)
+                item.Elapsed += Time.deltaTime;
+                if (item.INTERVAL <= item.Elapsed)
                 {
-                    case EnemyTableItem.EnemyID.ENEMY_RED: popEnemy = EnemyRed; break;
-                    case EnemyTableItem.EnemyID.ENEMY_GREEN: popEnemy = EnemyGreen; break;
-                    case EnemyTableItem.EnemyID.ENEMY_BLUE: popEnemy = EnemyBlue; break;
-                    default: return;
+                    item.Elapsed = 0;
+                    // 座標をランダムに決める
+                    Vector3 pos;
+                    pos.x = m_Camera.GetScreenRight() + SPRITE_SIZE / 2.0f;
+                    pos.y = Random.Range(m_Camera.GetScreenTop(), m_Camera.GetScreenBottom());
+                    pos.z = 0.0f;
+                    GameObject popEnemy = null;
+
+                    switch (item.ID)
+                    {
+                        case EnemyTableItem.EnemyID.ENEMY_RED: popEnemy = EnemyRed; break;
+                        case EnemyTableItem.EnemyID.ENEMY_GREEN: popEnemy = EnemyGreen; break;
+                        case EnemyTableItem.EnemyID.ENEMY_BLUE: popEnemy = EnemyBlue; break;
+                        default: return;
+                    }
+                    Instantiate(popEnemy, pos, Quaternion.identity, transform);
                 }
-                Instantiate(popEnemy, pos, Quaternion.identity, transform);
             }
         }
     }
--- Project/test2D/Assets/Item/ItemManager.cs
+++ Project/test2D/Assets/Item/ItemManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ItemManager : MonoBehaviour
 {
     private CameraController m_Camera = null;
+    private GameMainManager m_GameMain = null;
     static float SPRITE_SIZE = 32.0f;
 
     private SweetsPopTable m_Table = null;
@@ -15,6 +16,7 @@ public class ItemManager : MonoBehaviour
     void Start()
     {
         m_Camera = GameObject.Find("Main Camera").GetComponent<Camera>().GetComponent<CameraController>();
+        m_GameMain = GameObject.Find("GameManager").GetComponent<GameMainManager>();
 
         // 出現テーブルの0を読み込む
         LoadTable(0);
@@ -23,27 +25,31 @@ public class ItemManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 出現テーブルからアイテムを出現させる
-        foreach (SweetsTableItem item in m_Table.SweetsTableItemList)
+        // ゲームプレイ中のみ敵を出現させる
+        if (m_GameMain.state == GameMainManager.STATE.MAIN)
         {
-            item.Elapsed += Time.deltaTime;
-            if (item.INTERVAL <= item.Elapsed)
+            // 出現テーブルからアイテムを出現させる
+            foreach (SweetsTableItem item in m_Table.SweetsTableItemList)
             {
-                item.Elapsed = 0;
-                // 座標をランダムに決める
-                Vector3 pos;
-                pos.x = m_Camera.GetScreenRight() + SPRITE_SIZE / 2.0f;
-                pos.y = Random.Range(m_Camera.GetScreenTop(), m_Camera.GetScreenBottom());
-                pos.z = 0.0f;
-                GameObject popItem = null;
-                switch (item.ID)
+                item.Elapsed += Time.deltaTime;
+                if (item.INTERVAL <= item.Elapsed)
                 {
-                    case SweetsTableItem.SweetsID.CANDY: popItem = ItemCandy; break;
-                    case SweetsTableItem.SweetsID.PUDDING: popItem = ItemPudding; break;
-                    default: return;
+                    item.Elapsed = 0;
+                    // 座標をランダムに決める
+                    Vector3 pos;
+                    pos.x = m_Camera.GetScreenRight() + SPRITE_SIZE / 2.0f;
+                    pos.y = Random.Range(m_Camera.GetScreenTop(), m_Camera.GetScreenBottom());
+                    pos.z = 0.0f;
+                    GameObject popItem = null;
+                    switch (item.ID)
+                    {
+                        case SweetsTableItem.SweetsID.CANDY: popItem = ItemCandy; break;
+                        case SweetsTableItem.SweetsID.PUDDING: popItem = ItemPudding; break;
+                        default: return;
+                    }
+                    // リソースからロード
+                    Instantiate(popItem, pos, Quaternion.identity, transform);
                 }
-                // リソースからロード
-                Instantiate(popItem, pos, Quaternion.identity, transform);
             }
         }
     }
