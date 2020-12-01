4a108526190e7d8e2413cf8b9ad06066643a77c2
s-kosugi <program.sk5@outlook.jp>
2020-12-01 19:01:21 +0900

    通信待ち表記を表示するようにした

---
+++ Project/test2D/Assets/UI/ConnectWaitUI.cs
@@ -0,0 +1,41 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ConnectWaitUI : MonoBehaviour
+{
+    private PlayFabWaitConnect WaitConnect = null;
+    private TextMeshProUGUI text;
+    [SerializeField] float ConnectAnimeTime = 1.0f;
+    private float ConnectAnimeCounter = 0;
+
+    void Start()
+    {
+        WaitConnect = GameObject.Find("PlayFabManager").GetComponent<PlayFabWaitConnect>();
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if (WaitConnect.IsWait())
+        {
+            ConnectAnimeCounter += Time.deltaTime;
+            if (ConnectAnimeCounter <= ConnectAnimeTime / 3.0f)
+            {
+                text.text = "ちょっとまってね・";
+            }
+            else if (ConnectAnimeCounter <= ConnectAnimeTime / 3.0f * 2.0f)
+            {
+                text.text = "ちょっとまってね・・";
+            }
+            else
+            {
+                text.text = "ちょっとまってね・・・";
+            }
+        }
+        else
+        {
+            ConnectAnimeCounter = 0f;
+            text.text = "";
+        }
+    }
+}
