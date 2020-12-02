82dd423210f51238cadd071a84c7626e4a8d2266
s-kosugi <program.sk5@outlook.jp>
2020-12-02 11:13:41 +0900

    キャラクターに丸影オブジェクトを追加

---
+++ Project/test2D/Assets/GameMainObjectScript/ChaseShadow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseShadow : MonoBehaviour
+{
+    GameObject GroundObject;
+    [SerializeField] float OffsetY = 14f;
+    void Start()
+    {
+        GroundObject = GameObject.Find("Ground");
+    }
+
+    void Update()
+    {
+        // 地面の座標を持ってくる
+        this.transform.position = new Vector3(this.transform.position.x,GroundObject.transform.position.y + OffsetY, this.transform.position.z);
+    }
+}
