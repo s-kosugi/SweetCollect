d9b30b45345f801fafeb74cf49cb931994f49d23
merukuriusu <merukuri96usu23@gmail.com>
2020-12-03 19:36:53 +0900

    Ui関連 キャンバスから独立したPreviewの作成 そこで出た不具合の修正

---
+++ Project/test2D/Assets/UI/Shop/Clothing_Parent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clothing_Parent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
