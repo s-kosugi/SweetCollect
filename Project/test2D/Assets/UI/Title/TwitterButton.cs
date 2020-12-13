beda81be60a723f3819972d9bd3dc440e9e8fc9c
s-kosugi <program.sk5@outlook.jp>
2020-12-13 20:47:11 +0900

    ツイッター呟きボタンの機能を追加

---
+++ Project/test2D/Assets/UI/Title/TwitterButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TwitterButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PushButton()
+    {
+        //urlの作成
+        string esctext = UnityWebRequest.EscapeURL("テストツイート\nhttps://twitter.com/SweetCollectDev");
+        string esctag = UnityWebRequest.EscapeURL("スイートコレクト");
+        string url = "https://twitter.com/intent/tweet?text=" + esctext + "&hashtags=" + esctag;
+
+        //Twitter投稿画面の起動
+        Application.OpenURL(url);
+    }
+}
---
+++ Project/test2D/Assets/Utility/TwitterLogin.cs
@@ -0,0 +1,42 @@
+﻿using TwitterKit.Unity;
+using UnityEngine;
+
+public class TwitterLogin : MonoBehaviour
+{
+	[SerializeField] private string _AccessToken;
+	[SerializeField] private string _Secret;
+
+	void Start()
+	{
+		Twitter.Init();
+		this.TwitterAuth();
+	}
+
+	void Update() { }
+
+	public void TwitterAuth()
+	{
+		Debug.Log("[Info] : start login");
+		TwitterSession session = Twitter.Session;
+		if (session == null)
+		{
+			Twitter.LogIn(LoginComplete, LoginFailure);
+		}
+		else
+		{
+			LoginComplete(session);
+		}
+	}
+
+	public void LoginComplete(TwitterSession session)
+	{
+		Debug.Log("[Info] : Login success. " + session.authToken);
+		_AccessToken = session.authToken.token;
+		_Secret = session.authToken.secret;
+	}
+
+	public void LoginFailure(ApiError error)
+	{
+		Debug.Log("[Error ] : Login faild code =" + error.code + " msg =" + error.message);
+	}
+}
