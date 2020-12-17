e39ffc077732e52c57cff638ec2ea816388d984d
merukuriusu <merukuri96usu23@gmail.com>
2020-12-17 13:59:41 +0900

    チュートリアルのUi追加 Batu

---
+++ Project/test2D/Assets/UI/Tutorial/BatumageController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BatumageController : MonoBehaviour
+{
+    Image BatuImage = default;
+    [SerializeField] float StartScale = 3.0f;
+    [SerializeField] float AnimationTime = 2.0f;
+    [SerializeField] TutrialSceneManager tutrial = default;
+    float animationCounter = 0f;
+
+    void Start()
+    {
+        BatuImage = GetComponent<Image>();
+        BatuImage.color = new Color(255f, 255f, 255f, 0f);
+        BatuImage.transform.localScale = new Vector3(StartScale, StartScale);
+    }
+
+
+    void Update()
+    {
+        if (tutrial.tutrial == TutrialSceneManager.TUTRIAL.TUTRIAL_03 || tutrial.tutrial == TutrialSceneManager.TUTRIAL.TUTRIAL_04)
+        {
+
+            animationCounter += Time.deltaTime;
+            if (animationCounter >= AnimationTime)
+            {
+                animationCounter = 0f;
+            }
+
+            float scale = Easing.OutQuint(animationCounter, AnimationTime, 1f, StartScale);
+            BatuImage.transform.localScale = new Vector3(scale, scale);
+            float alpha = Easing.OutQuint(animationCounter, AnimationTime, 1f, 0f);
+            BatuImage.color = new Color(1f, 1f, 1f, alpha);
+        }
+    }
+}
---
+++ Project/test2D/Assets/UI/Tutorial/HideBatu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HideBatu : MonoBehaviour
+{
+    Image fingerImage = default;
+    [SerializeField] TutrialSceneManager tutrial = default;
+
+    void Start()
+    {
+        fingerImage = GetComponent<Image>();
+    }
+
+
+    void Update()
+    {
+
+        if (tutrial.tutrial == TutrialSceneManager.TUTRIAL.TUTRIAL_01 ||
+            tutrial.tutrial == TutrialSceneManager.TUTRIAL.TUTRIAL_02 ||
+            tutrial.tutrial == TutrialSceneManager.TUTRIAL.TUTRIAL_END
+            )
+        {
+            // 透明にする
+            fingerImage.color = new Color(1f, 1f, 1f, 0f);
+        }
+    }
+}
