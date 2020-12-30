52eb731fa69f84a3c4baaf5e16890bd2fa88014f
s-kosugi <program.sk5@outlook.jp>
2020-12-30 10:42:00 +0900

    Achievementアイテムから複数のカスタムデータを読み込めるように変更 LitJsonをプロジェクトに追加

--- Project/test2D/Assets/UI/Achivement/RewordImage.cs
+++ Project/test2D/Assets/UI/Achivement/RewordImage.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -42,8 +41,11 @@ public class RewordImage : MonoBehaviour
 
             if (catalogItem.CustomData != null)
             {
-                // ディクショナリにイメージを追加
-                previewDictionary.Add(store.StoreItems[i].ItemId, Resources.Load<Sprite>("Player\\" + catalogItem.CustomData));
+                // LitJsonを使ってJsonを連想配列化する
+                var jsonlist = LitJson.JsonMapper.ToObject<Dictionary<string, string>>(catalogItem.CustomData);
+
+                // 連想配列からデータを読み込み
+                previewDictionary.Add(store.StoreItems[i].ItemId, Resources.Load<Sprite>("Player\\" + jsonlist["REWORD"]));
             }
         }
     }
