05683fd4a2a0482823092e897b3f315154d5ae21
s-kosugi <program.sk5@outlook.jp>
2021-01-26 15:07:21 +0900

    リザルト時の演出を追加 エリアアウトチェックにそれぞれの方向を指定できるようにした。

---
+++ Project/test2D/Assets/Item/ResultItem/ResultItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ResultItem : MonoBehaviour
+{
+    [SerializeField] float rotateSpeedMin = 120.0f;
+    [SerializeField] float rotateSpeedMax = 360.0f;
+    float rotationSpeed = 0f;
+    float rotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationSpeed = Random.Range(rotateSpeedMin,rotateSpeedMax);
+        transform.rotation = Quaternion.Euler(0, 0, Random.Range(0f,360.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Z軸回転
+        rotation += rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0, 0, rotation);
+
+    }
+}
---
+++ Project/test2D/Assets/Item/ResultItem/ResultItemManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultItemManager : MonoBehaviour
+{
+    [SerializeField] float popInterval = 0.5f;
+    [SerializeField] float popMinX = -200.0f;
+    [SerializeField] float popMaxX = 200.0f;
+    [SerializeField] float popY = 200.0f;
+    [SerializeField] GameObject itemCandy = default;
+    [SerializeField] GameObject itemPudding = default;
+    [SerializeField] GameObject itemCake = default;
+    [SerializeField] GameObject itemTaiyaki = default;
+
+    float popCount = 0f;
+
+    enum ITEM_TYPE
+    {
+        CANDY,
+        PUDDING,
+        CAKE,
+        TAIYAKI,
+
+        MAX,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        popCount += Time.deltaTime;
+        if (popCount >= popInterval)
+        {
+            popCount = 0f;
+
+            ITEM_TYPE type = (ITEM_TYPE)Random.Range(0,(float)ITEM_TYPE.MAX);
+            GameObject obj = default;
+            switch (type)
+            {
+                case ITEM_TYPE.CANDY: obj = Instantiate(itemCandy,this.transform); break;
+                case ITEM_TYPE.PUDDING: obj = Instantiate(itemPudding, this.transform); break;
+                case ITEM_TYPE.CAKE: obj = Instantiate(itemCake, this.transform); break;
+                case ITEM_TYPE.TAIYAKI: obj = Instantiate(itemTaiyaki, this.transform); break;
+            }
+            if (obj != default)
+            {
+                obj.transform.position = new Vector3(Random.Range(popMinX,popMaxX),popY,this.transform.position.z);
+            }
+        }
+    }
+}
