c18acd8ae5825b762adccbd52ce3627d2c45d2b8
s-kosugi <program.sk5@outlook.jp>
2021-02-03 09:18:01 +0900

    お問い合わせメニューを作成した

---
+++ Project/test2D/Assets/UI/Title/Support/MailSendButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MailSendButton : MonoBehaviour
+{
+    public void PushButton()
+    {
+        string url = "mailto:sweetcollectdev@gmail.com";
+
+        //メールの起動
+        Application.OpenURL(url);
+    }
+}
---
+++ Project/test2D/Assets/UI/Title/Support/TwitterLinkButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TwitterLinkButton : MonoBehaviour
+{
+    public void PushButton()
+    {
+        string url = "https://twitter.com/SweetCollectDev";
+
+        //Twitter開発者画面の起動
+        Application.OpenURL(url);
+    }
+}
