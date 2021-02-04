b401ef021ec64e0db9052b6e2693b84bf9a458ab
s-kosugi <program.sk5@outlook.jp>
2021-02-04 12:34:36 +0900

    WEBGL版でツイッターアクセスを別タブに切り替える処理を作成。

--- Project/test2D/Assets/UI/Title/TwitterButton.cs
+++ Project/test2D/Assets/UI/Title/TwitterButton.cs
@@ -1,17 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
 using UnityEngine.Networking;
 
 public class TwitterButton : MonoBehaviour
 {
+    // Assets/Plugins/WebGLにあるOpenNewTabPluginをインポートする
+    [DllImport("__Internal")] private static extern void OpenNewTab(string URL);
+
     private ScoreManager scoreManager = default;
     void Start()
     {
-
-#if UNITY_WEBGL
-        // WEBGL版では非表示にする
-        Destroy(this.gameObject);
-#endif
-
         scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
 
     }
@@ -28,7 +26,13 @@ public class TwitterButton : MonoBehaviour
         string esctag = UnityWebRequest.EscapeURL("スイートコレクト");
         string url = "https://twitter.com/intent/tweet?text=" + esctext + "&hashtags=" + esctag;
 
-        //Twitter投稿画面の起動
+        // Twitter投稿画面の起動
+#if UNITY_EDITOR
         Application.OpenURL(url);
+#elif UNITY_WEBGL
+        OpenNewTab(url);
+#else
+        Application.OpenURL(url);
+#endif
     }
 }
