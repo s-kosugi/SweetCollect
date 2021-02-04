e684d46e92c723409c4864b98e91ea6d4cd54800
merukuriusu <merukuri96usu23@gmail.com>
2021-02-04 10:05:27 +0900

    実績ヒントが表記中にボタンを押せなくしました

---
+++ Project/test2D/Assets/UI/Shop/DisableShopFadeButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableShopFadeButton : MonoBehaviour
+{
+    [SerializeField] private DisableSceneFadeButton disable = null;
+    [SerializeField] private BuyAndWearButton buyAndWear = null;
+    Button button = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buyAndWear.GetState() == BuyAndWearButton.STATE.PREVIEWHINT || buyAndWear.GetState() == BuyAndWearButton.STATE.UPDATE)
+            button.enabled = false;
+        else
+            button.enabled = true;
+    }
+}
