ce5019522633892f35dffd46a37023ee0da7b08b
s-kosugi <program.sk5@outlook.jp>
2021-02-16 14:52:57 +0900

    一部ファイルの復旧

---
+++ Project/test2D/Assets/ArrangePattern/ArrangeTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ArrangeTable : ScriptableObject
+{
+    public List<ArrangePattern> ArrangeTableItemList = new List<ArrangePattern>();
+}
+
+[System.Serializable]
+public class ArrangePattern
+{
+    [SerializeField] public GameObject PatternPrefab;
+}
